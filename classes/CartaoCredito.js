1aa69646a4acf10586ffa73353c89c84e6315a4e
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-08-13 19:38:54 -0300

    removed: Cartao de Crédito

--- classes/CartaoCredito.js
+++
@@ -1,6 +0,0 @@
-class CartaoCredito extends Classes
-{
-    static table = 'cartao_credito';
-    static fields = [ 'id', 'convidado', 'nome', 'numero', 'ano', 'mes', 'cvv' ];
-}
-module.exports = CartaoCredito;
