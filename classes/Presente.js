fa280a03cbf908495e033a5eca394795a168ade8
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-08-14 22:05:05 -0300

    updated: presente migration

--- classes/Presente.js
+++ classes/Presente.js
@@ -1,7 +1,7 @@
 class Presente extends Classes
 {
     static table = 'presente';
-    static fields = [ 'id', 'convidado', 'produto', 'valor', 'forma_pagamento', 'recibo' ];
+    static fields = [ 'id', 'convidado', 'produto', 'valor', 'forma_pagamento', 'recibo', 'status' ];
 }
 
 module.exports = Presente;
