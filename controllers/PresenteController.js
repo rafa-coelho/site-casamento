542f017bb9715d3ae0d3c1645e1f082674f6519f
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-08-15 21:37:54 -0300

    wip

--- controllers/PresenteController.js
+++ controllers/PresenteController.js
@@ -69,4 +69,49 @@ module.exports = (app) => {
         res.send(resp);
     });
 
+    app.post(`/gift/:id/second`, async (req, res) => {
+        const { headers, params } = req;
+        const resp = {
+            status: 0,
+            data: null,
+            errors: []
+        };
+
+        if (!headers['authorization']) {
+            resp.errors.push({
+                msg: "Informe o código do convidado"
+            });
+            return res.status(403).send(resp);
+        }
+
+        const convidado = await Convidado.GetFirst(`code = '${headers['authorization']}'`);
+
+        if (!convidado) {
+            resp.errors.push({
+                msg: "Convidado não encontrado!"
+            });
+            return res.status(403).send(resp);
+        }
+
+        const presente = await Presente.GetFirst(`convidado = '${convidado.id}' AND id = '${params.id}'`);
+
+        if(!presente){
+            res.errors.push({
+                msg: "Presente não encontrado"
+            });
+            return res.status(404).send(resp);
+        }
+
+        if(presente.forma_pagamento !== "BOLETO"){
+            resp.errors.push({
+                msg: "Só é possível pedir segunda via de pagamentos realizados no boleto"
+            });
+            return res.status(400).send(resp);
+        }
+
+        const pag = new PagSeguro();
+
+        res.send(resp);
+    });
+
 };
