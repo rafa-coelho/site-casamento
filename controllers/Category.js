0863583c7eb77521e32c044065fff0101e9e4506
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-08-18 18:09:29 -0300

    updated: get categories response structure

--- controllers/Category.js
+++ controllers/Category.js
@@ -1,8 +1,35 @@
 module.exports = (app) => {
 
     app.get(`/category`, async (req, res) => {
+        const { headers, body } = req;
+        const resp = {
+            status: 0,
+            data: null,
+            errors: []
+        };
+        
+        if (!headers['authorization']) {
+            resp.errors.push({
+                msg: "Informe o código do convidado"
+            });
+            return res.status(403).send(resp);
+        }
+
+        const convidado = await Convidado.GetFirst(`code = '${headers['authorization']}'`);
+
+        if (!convidado) {
+            resp.errors.push({
+                msg: "Convidado não encontrado!"
+            });
+            return res.status(403).send(resp);
+        }
+
         const categories = await Categoria.Get();
-        res.send(categories);
+
+        resp.status = 1;
+        resp.data = categories;
+
+        res.send(resp);
     });
 
     app.get(`/category/:cat`, async (req, res) => {
