158e0e0fc07796db5fc58f1ff9182dc9071f514a
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-03-03 21:41:24 -0300

    updated

--- database/migrations/01_create_convidado.js
+++ database/migrations/01_create_convidado.js
@@ -1,20 +1,22 @@
 exports.up = async function (database, utf8 = false) {
-
-    return database.schema.createTable("convidado", table => {
-        if (utf8)
-            table.collate('utf8_unicode_ci');
-        table.string('id', 45).primary();
-        table.string('code', 10).notNullable();
-        table.string('nome', 80).notNullable();
-        table.string('email', 80);
-        table.string('whatsapp', 15);
-        table.int('quantidade').defaultTo(1);
-        table.int('quantidade_confirmado');
-        table.bool('padrinho').defaultTo(false);
-        table.bool('confirmado').defaultTo(false);
-        table.bool('deleted').defaultTo(false);
+    return database.schema.hasTable('convidado').then(function (exists) {
+        if (!exists)
+            return database.schema.createTable("convidado", table => {
+                if (utf8)
+                    table.collate('utf8_unicode_ci');
+                table.string('id', 45).primary();
+                table.string('code', 10).notNullable();
+                table.string('nome', 80).notNullable();
+                table.string('email', 80);
+                table.string('whatsapp', 15);
+                table.int('quantidade').defaultTo(1);
+                table.int('quantidade_confirmado');
+                table.bool('padrinho').defaultTo(false);
+                table.bool('confirmado').defaultTo(false);
+                table.bool('deleted').defaultTo(false);
+                console.log('teste')
+            });
     });
-
 
 }
 
