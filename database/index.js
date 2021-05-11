4f08e203ba240b029581bebd0723c2b651b1c8fa
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-05-11 16:49:23 -0300

    updated: prod db

--- database/index.js
+++ database/index.js
@@ -2,6 +2,6 @@ const knex = require('knex');
 const config = require('./config');
 
 require('dotenv').config();
-global.PROD = process.env.NODE_ENV == 'prod';
+global.PROD = process.env.NODE_ENV !== 'prod';
 
 module.exports = knex(config[PROD ? 'production' : 'development']);
