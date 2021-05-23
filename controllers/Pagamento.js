057849e42a7a3e3d027d7c5bb497ec5a265e1e3c
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-05-23 09:11:23 -0300

    added: debug

--- controllers/Pagamento.js
+++ controllers/Pagamento.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
         };
 
         var urlPG = process.env.NODE_ENV == 'prod' ? 'https://ws.pagseguro.uol.com.br/v2/sessions' : 'https://ws.sandbox.pagseguro.uol.com.br/v2/sessions';
-
+        console.log(`${urlPG}?email=${process.env.PS_EMAIL}&token=${process.env.NODE_ENV === 'prod' ? process.env.PS_TOKEN : process.env.PS_TOKEN_SB}`);
         const request = await axios.post(`${urlPG}?email=${process.env.PS_EMAIL}&token=${process.env.NODE_ENV === 'prod' ? process.env.PS_TOKEN : process.env.PS_TOKEN_SB}`);
 
         var response = await Hash.xml2json(request.data);
